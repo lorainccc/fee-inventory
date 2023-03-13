66b8b02fc0d1a355d50cc4ea5a72894c67ae0622
Thomas Jay Benjamin <tbenjamin@lorainccc.edu>
2023-03-13 11:54:26 -0400

    initial commit

---
+++ FI.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(knitr)
+
+fee_data <- read.csv("FY23_REVISED_FI (01-11-2023).csv") %>%
+    mutate(
+        amt_fmt = scales::dollar(Feeamount)
+    )
+
+fee_data %>%
+    filter(Feecategorycode == "SP", Feecode == "FI" | Feecode == "RB") %>%
+    select(c("Feetitle", "amt_fmt")) %>%
+    kable(align = "lr")
+
+fee_data %>%
+    filter(Feecategorycode == "SP", Feecode == "PF", Frequency == "OT") %>%
+    select(c("Academicprogram", "Feetitle", "amt_fmt")) %>%
+    kable(align = "llr")
+
+fee_data %>%
+    filter(Feecategorycode == "SP", Feecode == "IA") %>%
+    select(c("Courseid", "Feetitle", "amt_fmt")) %>%
+    kable(align = "llr")
+
+fee_data %>%
+    filter(Feecategorycode == "SP", Feecode == "CF" | Feecode == "LB") %>%
+    select(c("Courseid", "Feetitle", "amt_fmt")) %>%
+    arrange(Courseid) %>%
+    kable(align = "llr")
