b04ca3db875eb983a41be28dd813a12829984371
Thomas Jay Benjamin <tbenjamin@lorainccc.edu>
2023-08-24 16:25:56 -0400

    updates for 2023-24

--- FI.R
+++ FI.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 library(knitr)
 
-fee_data <- read.csv("FY23_REVISED_FI (01-11-2023).csv") %>%
+fee_data <- read.csv("FY24_FI.csv") %>%
     mutate(
         amt_fmt = scales::dollar(Feeamount)
     )
+
+fee_data %>%
+    filter(Feecode == "IN" | Feecode == "OD") %>%
+    select(c("Feetitle", "amt_fmt")) %>%
+    kable(align = "lr")
 
 fee_data %>%
     filter(Feecategorycode == "SP", Feecode == "FI" | Feecode == "RB") %>%
